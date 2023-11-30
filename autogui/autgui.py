c08300a437d6da6ce9bf7305f96e960446764204
Manillin <chri.vwm@gmail.com>
2023-11-30 12:40:12 +0100

    not so yolo

--- autogui/autgui.py
+++ autogui/autgui.py
@@ -1,20 +1,9 @@
 import pyautogui
 import time
 
-# Aspetta un attimo prima di iniziare
-# time.sleep(2)
-
-# Simula la pressione del tasto 'k'
-# pyautogui.press('k')
-v
-pyautogui.hotkey('command', 'v')
-
-
-L = 'command,v'
-
-l1, l2 = (str.split(L, ','))
-print(l1)
-print(l2)
-
-
-pyautogui.hotkey('command', 'm')
+time.sleep(2)
+# pyautogui.hotkey("command", "v", interval=0.25)
+S = 'command+m'
+c1, c2 = S.split('+')
+pyautogui.hotkey(c1, c2, interval=0.25)
+print()
