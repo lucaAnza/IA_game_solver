e6f60094cba970a2d174559aae7f1191c5cf8fb6
lucaAnza <anzaldi.luca@gmail.com>
2023-12-18 10:37:50 +0100

    Clean up

--- versione2/Main.py
+++ versione2/Main.py
@@ -11,78 +11,79 @@ import tracemalloc
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import pyautogui
 
-webhook_url = "https://discord.com/api/webhooks/1184788603470090320/jqkYHRC-y7P920AYLdB1e08g1WPLWzIcelEssk1tG23VXXE2kvgsNUQUhg5q7fYZ86hU"
+chri_webhook = "https://discord.com/api/webhooks/1184788603470090320/jqkYHRC-y7P920AYLdB1e08g1WPLWzIcelEssk1tG23VXXE2kvgsNUQUhg5q7fYZ86hU"
 luke_webhook = "https://discord.com/api/webhooks/1184915602431823944/3HyIjU1u40NnJUVpzzfBAEOAHZ9EZkiUwEeViffQLOxUwLpU25-dRR0-LJlx2snXbdsH"
 
-x_monitorChris = 6
+# Coordinate Luke
+"""
+x = 5
+y = 20
+side = 93
+"""
+
+# Coordinate Chrii
+x_global = 6
+y_global = 23
+side_global = 94
+
+    
+
+def webhook_print( url , title , description , img_name = None , color = 'ff0000' , author = None , trofei_screen = False):
+    webhook = DiscordWebhook(url=url)
+    embed = DiscordEmbed(title=title,
+                         description=description, color=color)
+    embed.set_timestamp()
+    webhook.add_embed(embed)
+
+    if(trofei_screen):
+        if(img_name != None):
+            screenBot.take_screenshot(870, 290, 490, 200, label='_dd_termination')  # Trofei screen
+            with open("./Screenshot/screenshot_dd_termination.png", "rb") as f:
+                webhook.add_file(file=f.read(), filename=img_name)
+    else:
+        if(img_name != None):
+            screenBot.take_screenshot(830, 60, 570, 960, label='_dd_termination')   # Full App screen
+            with open("./Screenshot/screenshot_dd_termination.png", "rb") as f: 
+                webhook.add_file(file=f.read(), filename=img_name)
+            
+    response = webhook.execute()   # Invio webhook
+
+    
+   
+    #embed.set_author(name="Very SAD Ferrets")
+    #embed.set_footer(text="TCP?")
+
 
 
 
 def failed_game_wh():
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title="Error",
-                         description="Partita non avviata per eccezione, script terminato", color="ff0000")
-    embed.set_author(name="Very SAD Ferrets")
-    embed.set_footer(text="TCP?")
-    embed.set_timestamp()
-    webhook.add_embed(embed)
-    response = webhook.execute()
+    webhook_print(chri_webhook , "Error" , "Partita non avviata per eccezione, script terminato" , color = 'ff0000')
+    
 
 
 def game_started_successfully():
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title="Success",
-                         description="Partita avviata correttamente", color="00ff00")
-    embed.set_author(name="Happy Ferrets")
-    embed.set_footer(text="TCP?")
-    embed.set_timestamp()
-    webhook.add_embed(embed)
-    response = webhook.execute()
+    webhook_print(chri_webhook , "Success" , "Partita avviata correttamente" , color = '00ff00')
 
 
 def check_error():
-    print_coloured.print_cyan_ts(
-        "Possibile conflitto trovato... going to sleep")
-    # webhook in caso di terminazione / soluzione momentanea
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title="Intervento",
-                         description="Situazione di stallo, going to sleep", color="ff0000")
-    embed.set_author(name="Ferrets")
-    embed.set_footer(text="TCP?")
-    embed.set_timestamp()
-    webhook.add_embed(embed)
-    response = webhook.execute()
+    print_coloured.print_cyan_ts("Possibile conflitto trovato... going to sleep")
+    webhook_print(chri_webhook , "Intervento" , "Situazione di stallo, going to sleep" , color = 'ff0000')
     time.sleep(8)
-    command = 'alt+3'
+    command = 'alt+3'  # Clicca il pulsante "continua a giocare"
     solverBot.send_input_gui(command)
-
-# 00FF00 - verde
-
 
 def stall_fixed():
     print_coloured.print_cyan_ts("Conflitto superato... continuo a giocare")
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title="Intervento",
-                         description="Conflitto superato... continuo a giocare! ", color="00ff00")
-    embed.set_author(name="Ferrets is happy")
-    embed.set_footer(text="TCP?")
-    embed.set_timestamp()
-    webhook.add_embed(embed)
-    response = webhook.execute()
+    webhook_print(chri_webhook , "Intervento" , "Conflitto superato... continuo a giocare! " , color = '00ff00')
+    
 
 
 def create_new_game():
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title="Iterazioni raggiunte",
-                         description="Chiusura partita attuale ", color="ff0000")
-    embed.set_author(name="Ferrets")
-    embed.set_footer(text="TCP?")
-    embed.set_timestamp()
-    webhook.add_embed(embed)
-    response = webhook.execute()
+    webhook_print(chri_webhook , "Iterazioni raggiunte" , "Chiusura partita attuale" , color = 'ff0000')
+    
     #Logica per riavere la matrice funzionale 
     time.sleep(20)
-    command = 'alt+3'
+    command = 'alt+3'  # Clicca il pulsante
     solverBot.send_input_gui(command)
     time.sleep(2)
 
@@ -93,11 +94,13 @@ def create_new_game():
     if immagine is None:
         print("Errore nel caricamento dell'immagine.")
         sys.exit()
-    matrix_number = analyseBot.get_matrix_item(immagine, type="Number", x=x_monitorChris, y=23, side=94)   # Chris-pc
+
+    matrix_number = analyseBot.get_matrix_item(immagine , type="Number" , x = x_global , y = y_global , side = side_global)  
+   
     counter = 0
-    slide_salagiochi = "alt+5"
-    play_new_game = "alt+6"
-    start_game = "alt+7"
+    slide_salagiochi = "alt+5"  # Imposta clicca "Spostati con la rotellina in giù"
+    play_new_game = "alt+6"    # Imposta clicca  "Avvia nuovo gioco"
+    start_game = "alt+7"     # Imposta clicca "Avvia partita"
     l_istruzioni = [slide_salagiochi, play_new_game, start_game]
 
     analyseBot.print_matrix(matrix_number)
@@ -110,45 +113,30 @@ def create_new_game():
         if immagine is None:
             print("Errore nel caricamento dell'immagine.")
             sys.exit()
-        matrix_number = analyseBot.get_matrix_item(immagine, type="Number", x=x_monitorChris, y=23, side=94)
-        if counter % 10 == 0:
+        matrix_number = analyseBot.get_matrix_item(immagine , type="Number" , x = x_global , y = y_global , side = side_global)   
+
+        if counter % 10 == 0:      # Debug
             print_coloured.print_green_ts("Partita in chiusura... attendere")
-        solverBot.send_input_gui('a+')
+        
+        solverBot.send_input_gui('a+')    # Esegue una mossa
         time.sleep(4)
         counter += 1
         
     # tempo esaurito, iniziare nuova partita
     print_coloured.print_green_ts("partita terminata! currently in the end game menu")
     time.sleep(5)
-    pyautogui.click(x=1110,y=939)
+    pyautogui.click(x=1110,y=939)   # Clicca il pulsante "Torna alla sala giochi"
     time.sleep(0.5)
-    pyautogui.click(x=1110,y=939)
+    pyautogui.click(x=1110,y=939)   # Clicca il pulsante "Torna alla sala giochi"  [fatto 2 volte per problemi con l'APP]
     
     for i in range(len(l_istruzioni)):
         time.sleep(5)
-        if i == 0:
-            webhookL = DiscordWebhook(url=luke_webhook)
-            embed = DiscordEmbed(title="Update",
-                     description=f"🏆 Trofei 🏆", color="03b2f8")
-            embed.set_timestamp()
-            webhookL.add_embed(embed)
-            screenBot.take_screenshot(870, 290, 490, 200, label='_dd_termination')
-            with open("./Screenshot/screenshot_dd_termination.png", "rb") as f:
-                webhookL.add_file(file=f.read(), filename="screenshot_update_trofei.png")
-            response = webhookL.execute()
+        if i == 0:   # Caso in cui ci troviamo in Sala Giochi
+            webhook_print(luke_webhook , "Update" , "🏆 Trofei 🏆" , color = '03b2f8' , img_name = "screenshot_update_trofei.png" , trofei_screen= True)
         solverBot.send_input_gui(l_istruzioni[i])
 
 
-
-try:
-    # Setting
-    os.system("color")  # abilita i colori nella shell
-
-    # Globals
-    num_righe = solverBot.num_righe
-    num_colonne = solverBot.num_colonne
-
-    def memory_stats(action="None"):
+def memory_stats(action="None"):
 
         MB = 1048576
 
@@ -160,6 +148,16 @@ try:
             current, peak = tracemalloc.get_traced_memory()
             print(
                 f"Istantanea = {int(current/MB)}Mb  ( {current}B )\n-----Picco = {int(peak/MB)}Mb  ( {peak}B )")
+
+
+
+try:
+    # Setting
+    os.system("color")  # abilita i colori nella shell
+
+    # Globals
+    num_righe = solverBot.num_righe
+    num_colonne = solverBot.num_colonne
 
     # MAIN
     print("\n\n")
@@ -193,22 +191,13 @@ try:
                 print_coloured.print_red_ts("")
                 print(f"{Fore.RED}{asc}{Style.RESET_ALL}")
                 create_new_game()
+                #Ripristino variabili ciclo
                 consecutive_error = 0
                 general_counter = 0
                 old_product = 0
                 game_started_successfully()
                 time.sleep(2)
-                #ScreenLuke
-                webhookL = DiscordWebhook(url=luke_webhook)
-                embed = DiscordEmbed(title="Update",
-                     description=f"Inizio partita", color="03b2f8")
-                embed.set_timestamp()
-                webhookL.add_embed(embed)
-                screenBot.take_screenshot(830, 60, 570, 960, label='_dd_termination')
-                with open("./Screenshot/screenshot_dd_termination.png", "rb") as f:
-                    webhookL.add_file(file=f.read(), filename="screenshot_partita_nuova.png")
-                response = webhookL.execute()
-                
+                webhook_print(chri_webhook , "Update" , "Inizio partita" , color = '03b2f8' , img_name="screenshot_partita_nuova.png")
             except Exception as e:
                 print_coloured.print_red_ts(e)
                 time.sleep(5)
@@ -224,7 +213,6 @@ try:
 
         # Cattura screenshot
         label = f'kz32'
-
         screenBot.take_screenshot(870, 330, 490, 620, label)
         #screenBot.take_screenshot(870, 330, 490, 620, f"{label}-FULLSCREENCHRIS", fullScreen=True) per salvare fullscreen
         img_name = f"Screenshot/screenshot{label}.png"
@@ -233,8 +221,7 @@ try:
             print("Errore nel caricamento dell'immagine.")
             sys.exit()
 
-        # matrix_number = analyseBot.get_matrix_item(immagine , type="Number" , x = 5 , y = 20 , side = 93)   # Lucas-pc
-        matrix_number = analyseBot.get_matrix_item(immagine , type="Number" , x = x_monitorChris , y = 23 , side = 94) 
+        matrix_number = analyseBot.get_matrix_item(immagine , type="Number" , x = x_global , y = y_global , side = side_global)  
 
         # Se almeno un elemento non l'ha riconosciuto [ prod == 0] non entra.
         if (analyseBot.checkMatrixProduct(matrix_number) != 0):
@@ -247,11 +234,10 @@ try:
             print(
                 f'{Fore.GREEN}Check della matrice andato a buon fine!{Style.RESET_ALL}')
 
-            if consecutive_error == 80:
+            if consecutive_error == 80:    # Se entra cui significa che è riuscito a risolvere lo stallo
                 stall_fixed()
-            consecutive_error = 0
-            # Esegue una mossa in base alla matrice di item dati in input
-            solverBot.scan_matrice(matrix_number)
+            consecutive_error = 0   
+            solverBot.scan_matrice(matrix_number)  # Esegue una mossa in base alla matrice di item dati in input
         else:
             print(f'{Fore.RED}Error {consecutive_error} {Style.RESET_ALL}')
             # Debug analisi
@@ -267,64 +253,25 @@ try:
         general_counter += 1
 
         # Controlli ogni tot cicli
-
-        if (general_counter % 500 == 0):     # Controllo memoria
-            memory_stats("Print")
-
         how_much = 2000
         if (general_counter % how_much == 0):
             # Controllo memoria
             memory_stats("Print")
+            
             # Update bot discord
+            webhook_print(luke_webhook , "Update" , "⬤ Complimenti hai raggiunto {general_counter} iterazioni!" , color = '03b2f8' , img_name='screenshot_{general_counter}_iterazioni.png')
             webhookL = DiscordWebhook(url=luke_webhook)
-            embed = DiscordEmbed(title="Update",
-                                 description=f"⬤ Complimenti hai raggiunto {general_counter} iterazioni!", color="03b2f8")
-            embed.set_timestamp()
-            webhookL.add_embed(embed)
-            screenBot.take_screenshot(
-                830, 60, 570, 960, label='_dd_termination')
-            with open("./Screenshot/screenshot_dd_termination.png", "rb") as f:
-                webhookL.add_file(
-                    file=f.read(), filename=f"screenshot_{general_counter}_iterazioni.png")
-            response = webhookL.execute()
             print_coloured.print_green_ts("Webhook sent!")
-
     memory_stats("Stop")
 
 
 except Exception as e:
     print(f"Errore{e}")
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title="Errore durante l'esecuzione",
-                         description=f'Error: {e}', color='ff0000')
-    embed.set_author(name="Il bro Ferretty")
-    embed.set_footer(text="TCP?")
-    embed.set_timestamp()
-    webhook.add_embed(embed)
-    webhook.execute()
+    webhook_print(chri_webhook , "Errore durante l'esecuzione" , "Error: {e}" , color = 'ff0000' )
     # Termina lo script
     sys.exit(1)
 
-# webhook in caso di terminazione / soluzione momentanea
-webhook = DiscordWebhook(url=webhook_url)
-embed = DiscordEmbed(title="Checkpoint",
-                     description="Script terminated... check terminal", color="03b2f8")
-embed.set_author(name="Il bro Ferretty")
-embed.set_footer(text="TCP?")
-embed.set_timestamp()
-webhook.add_embed(embed)
-response = webhook.execute()
+# webhook in caso di terminazione
+webhook_print(chri_webhook , "Checkpoint" , "Script terminated... check terminal" , color = '03b2f8' )
+webhook_print(luke_webhook , "Update" , "TERMINAZIONE" , color = '03b2f8' , img_name='screenshot_dd_termination.png')
 
-
-webhookL = DiscordWebhook(url=luke_webhook)
-embed = DiscordEmbed(title="Update",
-                     description=f"TERMINAZIONE", color="03b2f8")
-embed.set_timestamp()
-webhookL.add_embed(embed)
-screenBot.take_screenshot(830, 60, 570, 960, label='_dd_termination')
-with open("./Screenshot/screenshot_dd_termination.png", "rb") as f:
-    webhookL.add_file(file=f.read(), filename="screenshot_dd_termination.png")
-response = webhookL.execute()
-
-
-# ctrl+h
